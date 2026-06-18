478501f33b449ef0d6edb62ae2c67274d180d0ee
kananmuhtarov-89 <kanan.muhtarov@gmail.com>
2026-06-18 14:31:56 +0400

    Update sections.py

--- nvu/sections.py
+++ nvu/sections.py
@@ -36,7 +36,8 @@ def section6_top20_marka(df, col_marka):
     #t7 = tmp["Model (uyğunlaşdırılmış)"].dropna().value_counts().head(20).reset_index()
     #t7.columns = ["Model (uyğunlaşdırılmış)", "Say"]   #evvelki versiya
     #return t7
-def section7_topn_model(df, col_marka, col_model, n=20):
+
+def section7_top20_model(df, col_marka, col_model, n=20):
     tmp = df[[col_marka, col_model]].copy()
 
     tmp["Model (uyğunlaşdırılmış)"] = [
@@ -54,11 +55,6 @@ def section7_topn_model(df, col_marka, col_model, n=20):
 
     t7.columns = ["Model (uyğunlaşdırılmış)", "Say"]
     return t7
-
-
-# Köhnə çağırışlar pozulmasın deyə saxlayırıq
-def section7_top20_model(df, col_marka, col_model):
-    return section7_topn_model(df, col_marka, col_model, n=20)
 def section8_top20_reng(df, col_reng):
     tmp = df[col_reng].map(normalize_color)
     t8 = tmp.dropna().value_counts().head(20).reset_index()
