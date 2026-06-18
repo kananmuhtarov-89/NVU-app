a22a2212ae0fa8cb517d9e21e9eee96fe735adf9
kananmuhtarov-89 <kanan.muhtarov@gmail.com>
2026-06-18 14:27:11 +0400

    Update 8_7_Modellər_üzrə_Top_N_sayda.py

--- module1_pages/8_7_Modellər_üzrə_Top_N_sayda.py
+++ module1_pages/8_7_Modellər_üzrə_Top_N_sayda.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from nvu.sections import section7_topn_model
+from nvu.sections import section7_top20_model
+#from nvu.sections import section7_topn_model
 
 df_clean = st.session_state.get("df_clean")
 
@@ -12,6 +13,6 @@ TOP_MODEL = int(st.session_state.get("param_topN_model", 20))
 st.title(f"7) Top-{TOP_MODEL} Model (uyğunlaşdırılmış)")
 
 st.dataframe(
-    section7_topn_model(df_clean, "Marka", "Model", n=TOP_MODEL),
+    section7_top20_model(df_clean, "Marka", "Model", n=TOP_MODEL),
     use_container_width=True
 )
